8f0650c2b95a8742aeeb1683c3effcfa1a67a1f8
Xianyx <pushkar44320@gmail.com>
2024-04-08 22:18:19 +0530

    Some changes

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import bpy
+
+
+bl_info = {
+    "name": "Knife ex",
+    "author": "Xianyx",
+    "version": (0, 1),
+    "blender": (4, 1, 0),
+    "location": "View3D > Add Mesh",
+    "description": "Extends the Knife Tool functionality",
+    "category": "Mesh Editing",
+}
