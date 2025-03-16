16f54f9c10ef18581decc623161f4f2afb8f1fc7
Xianyx <pushkar44320@gmail.com>
2025-03-17 03:42:13 +0530

    errors got me f*cked up

--- __init__.py
+++ __init__.py
@@ -16,6 +16,8 @@ bl_info = {
 }
 
 
+
+
 class TestPanel(bpy.types.Panel):
     bl_label = "Test Panel"
     bl_idname = "PT_Test_Panel"
@@ -36,15 +38,13 @@ class TestPanel(bpy.types.Panel):
 
 
 class TestPanelB(bpy.types.Panel):
-    bl_label = 'Test Panel B'
-    bl_idname = 'PT_Test_Panel_B'
+    bl_label = "Test Panel B"
+    bl_idname = "PT_Test_Panel_B"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_category = 'Test Panel Tab'
-    bl_parent_id = 'PT_Test_Panel'
-    bl_options = {
-        'DEFAULT_CLOSED'
-    }
+    bl_category = "Test Panel Tab"
+    bl_parent_id = "PT_Test_Panel"
+    bl_options = {"DEFAULT_CLOSED"}
 
     def draw(self, context):
         layout = self.layout
@@ -69,16 +69,15 @@ class WM_OT_myop(bpy.types.Operator):
     text: bpy.props.StringProperty(name="Enter Text", default="")  # type: ignore # noqa: F811
     scale: bpy.props.FloatVectorProperty(name="scale Axis", default=(1, 1, 1))  # type: ignore
 
-
     def execute(self, context):
         bpy.ops.mesh.primitive_cube_add()
         obj = bpy.context.object
-        obj.name = self.text
-        obj.scale[0] = self.scale[0]
-        obj.scale[1] = self.scale[1]
-        obj.scale[2] = self.scale[2]
+        obj.name = self.text # type: ignore
+        obj.scale[0] = self.scale[0] # type: ignore
+        obj.scale[1] = self.scale[1] # type: ignore
+        obj.scale[2] = self.scale[2] # type: ignore
 
-        return {'FINISHED'}
+        return {"FINISHED"}
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
@@ -88,13 +87,13 @@ class WM_OT_Bounds_Toggle(bpy.types.Operator):
     bl_idname = "wm.bound_toggle"
     bl_label = "Toogle Bound View of the Active Object"
 
-    mode_options = ('EDIT', 'OBJECT')
+    mode_options = ("EDIT", "OBJECT")
 
     boundbool: bpy.props.BoolProperty(name="Boundry Toogle", default=False)  # type: ignore
 
     def execute(self, context):
         bpy.context.active_object.show_bounds = self.boundbool
-        bpy.ops.object.mode_set(mode='EDIT', toggle=True)
+        bpy.ops.object.mode_set(mode="EDIT", toggle=True)
 
         return {"FINISHED"}
 
@@ -107,29 +106,33 @@ class WM_BMesh_Test(bpy.types.Operator):
     bl_label = "Trying out the bmesh module"
 
     def subdiv_edit_toogle(self, context):
-        bpy.context.active_object.modifiers['Subdivision']
+        bpy.context.active_object.modifiers["Subdivision"]
 
     def execute(self, context):
         bpy.ops.object.shade_flat()
 
 
 class WM_Smooth_Toogle(bpy.types.Operator):
-    bl_idname = 'wm.smooth_toogle'
+    bl_idname = "wm.smooth_toogle"
     bl_label = "Not really intrested"
 
-    def smooth_toogle():
+    def smooth_toogle(): # type: ignore
         modifier_stack_data = bpy.context.active_object.modifiers.keys()
 
         # print(modifier_stack_data)
 
         for i in range(len(modifier_stack_data)):
-
-            if (bpy.context.active_object.modifiers[modifier_stack_data[i]].type == "SUBSURF"):
-                bpy.context.active_object.modifiers[modifier_stack_data[i]].show_in_editmode = False
+            if (
+                bpy.context.active_object.modifiers[modifier_stack_data[i]].type
+                == "SUBSURF"
+            ):
+                bpy.context.active_object.modifiers[
+                    modifier_stack_data[i]
+                ].show_in_editmode = False
                 bpy.ops.object.shade_flat()
 
     def execute(self, context):
-        self.smooth_toogle()
+        self.smooth_toogle() # type: ignore
 
         return {"FINISHED"}
 
@@ -166,8 +169,7 @@ def unregister():
     bpy.types.VIEW3D_MT_view.remove(new_bound_func)
 
 
-
 if __name__ == "__main__":
     register()
 
-    bpy.ops.wm.myop('INVOKE_DEFAULT')
+    bpy.ops.wm.myop("INVOKE_DEFAULT") # type: ignore
